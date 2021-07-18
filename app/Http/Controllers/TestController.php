d4a080cff1a210bd8e92f1c3a36508cd7834bbcc
Julker Nien Akib <35792784+jakib01@users.noreply.github.com>
2021-07-18 11:09:58 +0600

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,10 +10,11 @@ class TestController extends Controller
     public function state()
     {
         $data = DB::table('data')
-            ->select('state')->latest('state')->first();
-        $data2= response()->json($data);
-        $data2 = json_decode($data2, TRUE);
-        return $data2;
+            ->select('state')
+            ->latest('state')
+            ->first();
+//        $data = response()->json($data->state);
+        return $data->state;
 
     }
 
